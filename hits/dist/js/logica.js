6a41a69872a62a3d1973be9605a53e22093ad3d2
jorgev259 <jorgev259@outlook.es>
2016-07-22 14:56:13 -0600

    arreglo formulario Cancion y arreglos generales

---
+++ hits/dist/js/logica.js
@@ -0,0 +1,14 @@
+﻿function mostrarCancion() {
+
+
+
+    if (document.getElementById("gamer").style.display == "none") {
+        document.getElementById("gamer").style.display = "block";
+        document.getElementById("transparencia").style.display = "block";
+
+    } else {
+        document.getElementById("gamer").style.display = "none";
+        document.getElementById("transparencia").style.display = "none";
+    }
+
+}
