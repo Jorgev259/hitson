74ed049d8d5069780692a0c08eae4c68baea0677
jorgev259 <jorgev259@outlook.es>
2016-07-22 14:26:44 -0600

    ejemplo archivo

---
+++ hits/Controllers/fileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace hits_server.Controllers
+{
+    public class fileController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            var Request = HttpContext.Current.Request;
+            var file = Request.Files[0];
+            var path = HttpContext.Current.Server.MapPath(string.Format("~/temp"));
+            file.SaveAs(path + "/" + Request["ID"] + ".mp3");
+            return Ok(true);
+
+        }  
+    }
+}
