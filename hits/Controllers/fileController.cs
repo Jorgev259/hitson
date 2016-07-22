796cd94e8882765741264249030b494a07381083
jorgev259 <jorgev259@outlook.es>
2016-07-22 15:41:56 -0600

    gg

--- hits/Controllers/fileController.cs
+++ hits/Controllers/fileController.cs
@@ -17,8 +17,6 @@ namespace hits_server.Controllers
             var file = Request.Files[0];
             var path = HttpContext.Current.Server.MapPath(string.Format("~/temp"));
             file.SaveAs(path + "/" + Request["num"] + ".mp3");
-
-            Models
             return Ok(true);
 
         }  
